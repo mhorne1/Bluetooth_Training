eea349589926d54192d4049117676111bdfaf5f3
mhorne1 <29996075+mhorne1@users.noreply.github.com>
2021-02-09 17:48:50 -0800

    project_zero_cc2640r2lp_app

---
+++ c_embedded/cc2640r2/project_zero/project_zero_cc2640r2lp_app/FlashROM_StackLibrary/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -l"D:/TI/simplelink_cc2640r2_sdk_4_30_00_08/source/ti/devices/cc26x0r2/driverlib/bin/ccs/driverlib.lib" -l"D:/TI/simplelink_cc2640r2_sdk_4_30_00_08/source/ti/drivers/lib/drivers_cc26x0r2.aem3" -l"D:/TI/simplelink_cc2640r2_sdk_4_30_00_08/source/ti/display/lib/display.aem3" -l"D:/TI/simplelink_cc2640r2_sdk_4_30_00_08/kernel/tirtos/packages/ti/dpl/lib/dpl_cc26x0r2.aem3" -l"D:/UCI/CCS10_Workspace/project_zero_cc2640r2lp_stack_library/FlashROM_Library/project_zero_cc2640r2lp_stack_library.lib" -l"D:/UCI/CCS10_Workspace/project_zero_cc2640r2lp_stack_library/FlashROM_Library/ble_r2.symbols" -l"D:/UCI/CCS10_Workspace/project_zero_cc2640r2lp_stack_library/FlashROM_Library/lib_linker.cmd" -l"D:/TI/simplelink_cc2640r2_sdk_4_30_00_08/source/ti/blestack/common/cc26xx/ccs/cc26xx_app.cmd" -llibc.a
+
